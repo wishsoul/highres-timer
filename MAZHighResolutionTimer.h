9be524482110f4c5b4ab673c7e820e1f330a20c7
Michael Hanna <mykelkim@yahoo.com>
2014-11-30 18:08:02 -0500

    Create MAZHighResolutionTimer.h

---
+++ MAZHighResolutionTimer.h
@@ -0,0 +1,25 @@
+//
+//  MAZHighResolutionTimer.h
+//  mach_wait_until
+//
+//  Created by Michael Hanna on 2014-11-30.
+//  Copyright (c) 2014 maz.me. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MAZHighResolutionTimerDelegate <NSObject>
+
+@required
+- (void)highResolutionTimerDidFire;
+
+@end
+@interface MAZHighResolutionTimer : NSObject
+@property (nonatomic) uint64_t interval;
+@property (strong, nonatomic) id <MAZHighResolutionTimerDelegate> delegate;
+- (instancetype)initWithInterval:(uint64_t)milliseconds delegate:(id<MAZHighResolutionTimerDelegate>)delegate NS_DESIGNATED_INITIALIZER;
++ (instancetype)startWithInterval:(uint64_t)milliseconds delegate:(id<MAZHighResolutionTimerDelegate>)delegate;
+- (void)start;
+- (void)stop;
+
+@end
